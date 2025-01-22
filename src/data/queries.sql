95e6221e9b40ca5d62628b5c5bc1b4300c319b7b
Tony H <tony@hirvirinne.fi>
2025-01-22 17:05:05 +0200

    queries.sql tehty kyselyjä

---
+++ src/data/queries.sql
@@ -0,0 +1,16 @@
+-- SELECT * FROM public."Customers";
+
+-- SELECT COUNT(*) AS "Total orders" FROM public."Orders"; -- Total number of orders
+
+-- SELECT SUM(price_at_purchase * quantity) AS "Total sales" FROM public."Order_Items"; -- Total sales (sum of price_at_purchase * quantity from Order_Items)
+
+-- SELECT COUNT(*) AS "Count of products with low stock" FROM public."Products" WHERE stock_quantity < 10; -- Count of products with low stock (e.g., stock_quantity < 10)
+
+-- SELECT AVG(quantity * price_at_purchase) AS "Average order value", order_id FROM public."Order_Items" GROUP BY order_id ORDER BY "Average order value" DESC; -- Average order value
+
+-- SELECT  SUM(quantity * price_at_purchase) AS "Total sales per category", category FROM public."Order_Items", public."Products" GROUP BY category;  -- Total sales per category
+
+SELECT COUNT(OI.order_id) AS "Orders", SUM(price_at_purchase * quantity) AS "Total sales", order_date 
+FROM public."Order_Items" AS OI, public."Orders" AS O 
+GROUP BY order_date 
+ORDER BY order_date; -- Monthly breakdown of the number of orders and total sales
