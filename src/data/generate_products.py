b7d4f5cf7465b021437768b2eec8869be0449040
Miska Savenius <miska.savenius@gmail.com>
2025-01-22 14:14:53 +0200

    Kesken edelleen

--- src/data/generate_products.py
+++ src/data/generate_products.py
@@ -14,13 +14,45 @@ with psycopg2.connect(**config()) as conn:
             supplier_id_list.append(id[0])
 print(supplier_id_list)
 
-category_list = ['Electronics', 'Clothing', 'Home', 'Garden', 'Beauty', 'Health', 'Toys', 'Grocery', 'Sports', 'Outdoors', 'Automotive', 'Industrial', 'Books', 'Music', 'Movies', 'Games', 'Jewelry', 'Pet', 'Baby', 'Food', 'Handmade', 'Computers', 'Software', 'Tools', 'Shoes', 'Accessories', 'Luggage', 'Furniture', 'Office', 'School', 'Art', 'Crafts', 'Collectibles', 'Vintage', 'Antiques', 'Miscellaneous']
+category_list = ['Electronics', 'Home', 'Garden', 'Beauty', 'Health', 'Toys', 'Grocery', 'Music', 'Automotive']
+electronics = ['Cell Phones', 'Computers', 'Tablets', 'Cameras', 'Televisions', 'Headphones', 'Speakers', 'Smart Watches', 'Printers', 'Scanners', 'Accessories']
+music = ['CDs', 'Vinyl', 'Cassettes', 'Instruments', 'Sheet Music', 'Accessories']
+home = ['Furniture', 'Appliances', 'Decor', 'Lighting', 'Bedding', 'Bath', 'Patio', 'Garden', 'Storage', 'Cleaning', 'Tools', 'Safety', 'Heating', 'Cooling', 'Air Quality', 'Smart Home', 'Pet Supplies']
+garden = ['Plants', 'Seeds', 'Tools', 'Decor', 'Pots', 'Planters', 'Watering', 'Pest Control', 'Outdoor Furniture', 'Grills', 'Outdoor Cooking', 'Patio Furniture', 'Outdoor Storage', 'Sheds', 'Outdoor Power Equipment', 'Gardening', 'Landscaping', 'Pools', 'Spas', 'Hot Tubs', 'Outdoor Lighting', 'Fire Pits', 'Outdoor Heating', 'Outdoor Decor', 'Outdoor Structures', 'Fencing', 'Gates', 'Decking', 'Outdoor Cooking', 'Outdoor Recreation', 'Outdoor Games']
+beauty = ['Makeup', 'Skin Care', 'Hair Care', 'Fragrance', 'Bath', 'Body', 'Tools']
+health = ['Vitamins', 'Supplements', 'Diet', 'Nutrition', 'Sports', 'Fitness', 'Weight Loss', 'Health Care', 'Personal Care', 'Sexual Wellness', 'Medical Supplies', 'Equipment']
+toys = ['Action Figures', 'Dolls', 'Games', 'Puzzles', 'Building Sets', 'Outdoor Toys', 'Ride-Ons', 'Arts', 'Crafts', 'Learning', 'STEM', 'Plush', 'Stuffed Animals', 'Electronics', 'Remote Control', 'Collectibles', 'Video Games', 'Gaming', 'Consoles', 'Accessories']
+automotive = ['Parts', 'Accessories', 'Tools', 'Equipment', 'Car Care', 'Motorcycle', 'Powersports', 'Truck', 'RV', 'Tires', 'Wheels', 'Vehicles', 'Boats', 'Watercraft']
+grocery = ['Pantry', 'Snacks', 'Breakfast', 'Coffee', 'Tea', 'Beverages', 'Meal Kits', 'Specialty Diets', 'Gourmet Foods', 'Fresh Food', 'Frozen Food', 'Baby Food', 'Pet Food', 'Wine', 'Beer', 'Spirits', 'Alcohol']
+
+
 products_list = []
 for i in range(0, 100):
+    category = choice(category_list)
+    match category:
+        case 'Electronics':
+            product = choice(electronics)
+        case 'Music':
+            product = choice(music)
+        case 'Home':
+            product = choice(home)
+        case 'Garden':
+            product = choice(garden)
+        case 'Beauty':
+            product = choice(beauty)
+        case 'Health':
+            product = choice(health)
+        case 'Toys':
+            product = choice(toys)
+        case 'Automotive':
+            product = choice(automotive)
+        case 'Grocery':
+            product = choice(grocery)
+
     supplier = {
-        'name': fake.name(),
-        'category': choice(category_list),
-        'price': fake.country(),
+        'name': product,
+        'category': category,
+        'price': round(uniform(0.0, 9999.0),2),
         'supplier_id': choice(supplier_id_list),
         'stock_quantity': round(uniform(0.0, 100.0),2)
     }
